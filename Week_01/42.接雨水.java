190c0e0b1216c68b8eedbf21e9e3af0d0f46db08
kmadmin <24733573@qq.com>
2020-09-27 19:28:44 +0800

    Add files via upload

---
+++ Week_01/42.接雨水.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=java
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+class Solution {
+    public int trap(int[] height) {
+        int ans = 0;
+        int size = height.length;
+        for (int i = 1; i < size - 1; i++) {
+            int max_left = 0, max_right = 0;
+            for (int j = i; j >= 0; j--) { //Search the left part for max bar size
+                max_left = Math.max(max_left, height[j]);
+            }
+            for (int j = i; j < size; j++) { //Search the right part for max bar size
+                max_right = Math.max(max_right, height[j]);
+            }
+            ans += Math.min(max_left, max_right) - height[i];
+            //System.out.println("ans is " + ans + " i= " +i);
+        }
+        return ans;
+      }
+}
+// @lc code=end
+
